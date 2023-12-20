04c468ac982871ed01548413ae46100ea84b49ae
vigrodeepsa <143414659+vigrodeepsa@users.noreply.github.com>
2023-12-20 10:04:27 +0200

    Add files via upload

---
+++ js/image.js
@@ -0,0 +1,35 @@
+
+var currentImage = 44;
+var totalImages = 44;
+
+function changeImage(direction) {
+    currentImage += direction;
+    
+    if (currentImage < 1) {
+        currentImage = totalImages;
+    } else if (currentImage > totalImages) {
+        currentImage = 1;
+    }
+    
+
+    document.getElementById("image").src = "./images/stories/2024/" + currentImage + ".png";
+
+
+}
+
+document.getElementById("prev").addEventListener("click", function() {
+    changeImage(1);// i changed it from -1 to 1 so that it starts with new ones
+});
+
+document.getElementById("next").addEventListener("click", function() {
+    changeImage(-1);// i changed it from 1 to -1 so that it starts with new ones
+});
+
+document.getElementById("download").addEventListener("click", function() {
+    var link = document.createElement('a');
+    link.href = document.getElementById("image").src;
+    link.download = "image.png";
+    link.click();
+});
+
+
