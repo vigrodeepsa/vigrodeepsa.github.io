72fee14c10182560442a3f59f078bc3ba4654502
vigrodeepsa <143414659+vigrodeepsa@users.noreply.github.com>
2024-01-04 02:08:42 +0200

    Add files via upload

--- js/motivation-data.js
+++ js/motivation-data.js
@@ -52,7 +52,27 @@
       {
         date: '2023-12-30',
         post: "🌟 courage defines triumph! 💫💪 ' success is not final, failure is not fatal: it is the courage to continue that counts.'🚀✨ these powerful words remind us that success isn't the end and failure isn't the end-all-be-all. it's the resilience to persist that truly matters! 💡💖.",
-      }
+      },
+      {
+        date: '2023-12-31',
+        post: "don't forget to pray for a better new year🌟",
+      },
+      {
+        date: '2024-01-01',
+        post: "Goodbye to all the drama & pain of 2023. Hello to the Peace, Joy & Miracles of 2024.",
+      },
+      {
+        date: '2024-01-02',
+        post: "People Isolate Themselves For Bigger Reasons Than They Care To Explain. Give Em Space",
+      },
+      {
+        date: '2024-01-03',
+        post: "Get RID Of Everybody That Play BOTH sides.✂️ 2024",
+      },
+      {
+        date: '2024-01-04',
+        post: "To Create Generational Wealth, You Have To Take Risks And Move Differently.",
+      },
       // Add more posts here
     ];
     
